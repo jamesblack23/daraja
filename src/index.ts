4ae04d83a2ecd2e5d0446565bcbbb86d54a92e8d
Austine Were <austinewuncler@gmail.com>
2018-11-13 10:51:08 +0300

    Create DarajaBuilder constructor

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
-export default class DarajaBuilder {
+/**
+ *
+ *
+ * @export
+ * @class DarajaBuilder
+ */
+export class DarajaBuilder {
+  /**
+   * Creates an instance of DarajaBuilder.
+   * @param {number} shortcode - This is the organization's shortcode
+   * (Paybill or Buygoods - A 5 to 6 digit account number) used to identify an organization
+   * @param {string} consumerKey - Your App's Consumer Key (obtain from Developer's portal)
+   * @param {string} consumerSecret - Your App's Consumer Secret (obtain from Developer's portal)
+   */
   constructor(
     public shortcode: number,
     public consumerKey: string,
--- test/daraja.test.ts
+++ test/daraja.test.ts
@@ -1,5 +1,5 @@
 import { assert } from 'chai';
-import DarajaBuilder from '../src';
+import { DarajaBuilder } from '../src';
 
 describe('DarajaBuilder', () => {
   let builder: DarajaBuilder;
