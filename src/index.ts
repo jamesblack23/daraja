7c12f364a6d42685d791e398a41899197b5ce124
Austine Were <austinewuncler@gmail.com>
2018-11-13 10:27:47 +0300

    Configure builder constructor

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export default class DarajaBuilder {
+  constructor(
+    public shortcode: number,
+    public consumerKey: string,
+    public consumerSecret: string
+  ) {}
+}
---
+++ test/daraja.test.ts
@@ -0,0 +1,33 @@
+import { assert } from 'chai';
+import DarajaBuilder from '../src';
+
+describe('DarajaBuilder', () => {
+  let builder: DarajaBuilder;
+
+  beforeEach(() => {
+    builder = new DarajaBuilder(12345, 'key', 'secret');
+  });
+
+  describe('constructor', () => {
+    it('should set the respective properties', () => {
+      assert.propertyVal(
+        builder,
+        'shortcode',
+        12345,
+        'shortcode value not set'
+      );
+      assert.propertyVal(
+        builder,
+        'consumerKey',
+        'key',
+        'consumerKey value not set'
+      );
+      assert.propertyVal(
+        builder,
+        'consumerSecret',
+        'secret',
+        'consumerSecret value not set'
+      );
+    });
+  });
+});
